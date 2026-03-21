9177661d3cfd29b5c78d36c51de7e8417338951d
Antony Premkumar <antony_premkumar@outlook.com>
2026-03-21 01:18:54 +0100

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://computational-fluid-dynamics.com',
 	integrations: [mdx(), sitemap()],
 });
